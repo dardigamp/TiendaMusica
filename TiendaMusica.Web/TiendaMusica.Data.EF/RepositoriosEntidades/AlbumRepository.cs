d3abdbd5ba1ccb31c115ece67503157463c4dee9
Darwin Díaz Garrampié <dardigamp@gmail.com>
2016-05-25 00:52:52 -0500

    Publicación de TiendaMusica WEb: Edición para actualización de Albums

---
+++ TiendaMusica.Web/TiendaMusica.Data.EF/RepositoriosEntidades/AlbumRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TiendaMusica.Data.Repositorio;
+using TiendaMusica.Dominio;
+
+namespace TiendaMusica.Data.EF.RepositoriosEntidades
+{
+    class AlbumRepository :  GenericRepository<EFTiendaMusicaRepository, Album>
+    {
+        public AlbumRepository(EFTiendaMusicaRepository contexto) : base(contexto)
+        {
+
+        }
+    }
+}
---
+++ TiendaMusica.Web/TiendaMusica.Web/Controllers/ArtistasController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using TiendaMusica.Infraestructura;
+using TiendaMusica.Logica;
+using TiendaMusica.Logica.ViewModels;
+
+namespace TiendaMusica.Web.Controllers
+{
+    public class ArtistasController : Controller
+    {
+        private readonly TiendaConsultas tienda;
+        public ArtistasController()
+        {
+            tienda = ConstructorServicios.TiendaConsultas();
+        }
+        public ActionResult Albums(string nombre)
+        {
+            //Logica de Negocio no va en un controlador
+            IEnumerable<AlbumsPorArtistaViewModel> albums = tienda.Albums(nombre);
+            return View(albums);
+        }
+    }
+}
